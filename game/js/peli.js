d4ba0d19b709d55b94f9fbdbb7a1ef079b8a030f
eplehton <esko.lehtonen@helsinki.fi>
2014-08-12 11:49:46 +0300

    1. versio annotaatiotyökalusta
    Annotaatiotyökalu jolla luoda, edioida ja tallentaa targetteja. Hakee ne
    localStoragesta.
    
    export ei vielä oikeasti toimi.

--- game/js/peli.js
+++ game/js/peli.js
@@ -1,38 +1,139 @@
 function Shot(x, y, t, src){
-    this.x = x;
-    this.y = y;
-    this.t = t;
+    this.x = x
+    this.y = y
+    this.t = t
     this.src = src;
-};
+}
 
-function Target(src){
+function Target(str=null){
+     //if (str == null) {
+    this.id = Date.now();
     this.x = [];
     this.y = [];
     this.t = [];
-    this.src = src;
-};
+    this.end_t = Infinity;
+    /*} else {
+        var parts = str.split(';');
+        var id = parts[0];
+        var t = parts[1].split(',');
+        var x = parts[2].split(',');
+        var y = parts[3].split(',');
+        var end_t = parts[4];
+        
+        this.id = parseInt(id);
+        this.t = t.map(parseFloat);
+        this.x = x.map(parseFloat);
+        this.y = y.map(parseFloat);
+        this.end_t = parseFloat(end_t)
+    }*/
+}
 
-Target.prototype.addPoint = function(x, y, t){
-    this.x.push(x);
-    this.y.push(y);
-    this.t.push(t);
-};
+addTargetPoint = function(trg, x, y, t) {
+
+    var loc = findInsertIndex(trg, t);
+    trg.splice(loc, 0, t);
+    trg.splice(loc, 0, x);
+    trg.splice(loc, 0, y);
+}
+delTargetEnd = function(trg) {
+    console.log("delTargetEnd")
+    trg.end_t = Infinity;
+}
+
+delTargetPoint = function(trg, loc) {
+    console.log("delTargetPoint")
+    trg.x.splice(loc, 1);
+    trg.y.splice(loc, 1);
+    trg.t.splice(loc, 1);
+}
+
+/*Target.prototype.toString = function() {
+    var s = "" + this.id
+    s += ';' + this.t.map( function(x) { return x.toPrecision(3); }).join();
+    s += ';' + this.x.map( function(x) { return x.toPrecision(3); }).join();
+    s += ';' + this.y.map( function(x) { return x.toPrecision(3); }).join();
+    s += ';' + this.end_t.toPrecision(3);
+    return s
+}*/
+
+Target.prototype
 
 var points = 0;
 
 var shots = [];
 var correct_shots = [];
 
-var targets = [[[801,259, 3.789], [615, 259, 9.877]],  
+/* var targets = [[[801,259, 3.789], [615, 259, 9.877]],  
 [[434, 292, 2.224], [506, 284, 6.623], [778, 272, 12.407999], [380, 337, 14.664], [490, 302, 17.351], [582, 300, 19.487],[385, 283, 21.015], [630, 298, 21.968], [671, 264, 23.345], [819, 271, 26.103999], [435, 297, 29.072], [487, 296, 29.967], [854, 302, 33.64], [871, 275, 35.255999] ] , 
 [[517, 246, 6.983], [759, 246, 8.502999], [385, 261, 10.289]], 
 [[602, 291, 5.647], [795, 255, 12.535999], [789, 272, 17.4]], 
 [ [618, 282, 9.396], [478, 285, 12.947], [368,308, 14.958]],
 [ [789, 284, 18.063], [621, 260, 26.302999]]];
+*/
+
+
+function findInsertIndex(arr, val) {
+    /*
+        Assuming a sorted array arr, return the index where the val 
+        should be inserted in order to keep the arr sorted.
+        Helper function for keeping the targets sorted in time. 
+    */
+    var ipnt = arr.length;
+    for (var i=0; i<arr.length; i++) {
+        if (val < arr[i]) { 
+            ipnt = i;
+            break;
+        } 
+    }
+    return ipnt;
+}
+
+
+
+// Targets loading saving functions
+
+
+function loadLocalTargets() {
+    // Load targets from JSON data stored on localstorage, from item "targets"
+    var targets = {};
+    var targets_s = localStorage.getItem("targets");
+    console.log("targets_s: " + targets_s);
+    if (targets_s != null) { // if not available, make empty dict
+        targets = JSON.parse(targets_s);
+    } 
+    return targets;
+}
+
+function saveLocalTargets(all_targets) {
+    // save targets to localStorage as JSON
+    localStorage.setItem("targets", JSON.stringify(all_targets))
+}
+
+function getSourceTargets(all_targets, src) {
+    
+    // Helper function to get an array of targets for the specific video
+    // The file name of the clip is parsed, and used as the key to pick the right targets, regardless of
+    // the path of the clip.
+    // If the clipname is not present, an empty array is returned for convenience. 
+    var clipname = src.split("/").pop();
+    var src_trgs = all_targets[clipname];
+    console.log(src +" - "+ clipname + " - " + src_trgs);
+    if (typeof src_trgs == "undefined") { // if empty
+        src_trgs = [];
+    }
+    
+    console.log(src_trgs);
+    return src_trgs;
+}
 
 var clipset_pos = 0;
 
-var clipset =  [ ["1", "clips/potkulautailija.mp4"] , ["2", "clips/isoroba.mp4"],["3", "clips/jalankulkija_ja_suojatie.mp4" ] , ["4", "clips/jalankulkijat2.mp4"], ["5", "clips/vastaantulijat.mp4"], ["6", "clips/ratikka.mp4"]];
+var clipset =  [ ["1", "../clips/potkulautailija.mp4"] , 
+                 ["2", "../clips/isoroba.mp4"], 
+                 ["3", "../clips/jalankulkija_ja_suojatie.mp4" ] , 
+                 ["4", "../clips/jalankulkijat2.mp4"], 
+                 ["5", "../clips/vastaantulijat.mp4"], 
+                 ["6", "../clips/ratikka.mp4"]];
 
 var correct_hit_interval = 2.5;
    
@@ -227,15 +328,31 @@ function dataUlos() {
     };
 };
 
+function client2Rel(x, y) {
+    var vplayer = document.getElementById("videoplayer");
+    //console.log(x + " " + y + " " + vplayer.offsetLeft +" " + vplayer.offsetTop + " " + vplayer.offsetWidth + " " + vplayer.offsetHeight)
+    var relx = (x - vplayer.offsetLeft) / vplayer.offsetWidth
+    var rely = (y - vplayer.offsetTop) / vplayer.offsetHeight
+    return [relx, rely]
+}   
+
+function rel2Video(relx, rely) {
+    var vplayer = document.getElementById("videoplayer");
+    var x = relx * vplayer.offsetWidth + vplayer.offsetLeft
+    var y = rely * vplayer.offsetHeight + vplayer.offsetTop
+    return [x, y]
+}   
+
+
 function video2rel(hpposX,hpposY){
     var vplayer = document.getElementById("videoplayer");
-    var width=vplayer.offsetWidth;
-    var height=vplayer.offsetHeight;
+    var width= vplayer.offsetWidth;
+    var height = vplayer.offsetHeight;
     var relativeY = hpposY/height;
     var relativeX = hpposX/width;
     
     //return "Relative height " +relativeH +" and relative width " +relativeW;
-    return [relativeX,relativeY];
+    return [relativeX, relativeY];
 }
 
 function rel2video(hprelX,hprelY){ 
@@ -246,5 +363,5 @@ function rel2video(hprelX,hprelY){
     var absoluteX = hprelX*width;
     
     //return "Absolute height " + absoluteH +" and absolute width " + absoluteW;
-    return absoluteX,absoluteY;
+    return [absoluteX, absoluteY];
 }
