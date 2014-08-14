e555c91cfb46cfc564221f692e6ef4b4fbffdf74
plahteenlahti <perttu.lahteenlahti@gmail.com>
2014-08-14 11:47:16 +0300

    Pullaa tämä
    Kirjautuminen tiputettu pois ja siiretty omaan popuppiin. Scriptit
    vielä hieman hakusessa ja kenttänäkymän korjaan illalla.

--- game/js/PopUp.js
+++ game/js/PopUp.js
@@ -1,13 +1,25 @@
 $(function(){
     $('#alkuohje').hide();
     $('.ohjeruutu').hide();
+    $('#kirjautumiskentta').hide();
     
     $('#ohje').click(function(){
         $('.ohjeruutu').fadeIn(600).show();
     });
+    
     $('.ohjeruutu').click(function(){
         $(this).fadeOut(600, function(){
             $(this).hide();
         });
     });
+    
+    $('#kirjaudu').click(function(){
+        $('#kirjautumiskentta').fadeIn(600).show();
+    });
+    $('#kirjautumiskentta').click(function(){
+        $(this).fadeOut(600, function(){
+            $(this).hide();
+        });
+    });
+
 });
