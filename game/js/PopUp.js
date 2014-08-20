964031efda39f0f5d9675cada01c7c9816166940
paleface_91@hotmail.com <paleface_91@hotmail.com>
2014-08-20 12:29:04 +0300

    Pop-up- ja index-sivulta poistettu turhat funktiot, nyt kaikki PopUpissa paitsi yksi document.ready-funktio index-sivulla

--- game/js/PopUp.js
+++ game/js/PopUp.js
@@ -1,36 +1,44 @@
-$(function(){
+	$(function(){
     $('.ohjeruutu').hide();
     $('#kirjautumiskentta').hide();
     
+	
     $('#ohje').click(function(){
         $('.ohjeruutu').fadeIn(600).show();
     });
     
+	
     $('.ohjeruutu').click(function(){
         $(this).fadeOut(600, function(){
             $(this).hide();
         });
     });
+	
     
-    $('#kirjaudu').click(function(){
-        $('#kirjautumiskentta').fadeIn(600).show();
-    });
-    $('#kirjaudu').click(function(){
-        $('#kirjautumiskenttä').fadeOut(600, function(){
-            $(this).hide();
-        });
-    });
-    
-    
-
-    $("#kirjautumisruutu").on('submit', function(e) {
-        if (($("#kayttaja").val() == "") || ($("#salasana").val() == "")) {
-            return false;				/*tekee sen että ei voi kirjautua tyhjällä käyttäjänimikentällä */
-        } else {
-            tallennaNimi(($("#kayttaja").val()));
-        };
-    });
-    
+/* 	$(document).ready(function(){	
+				
+    var player_id = sessionStorage.getItem("player_id");        //Tämä on document.ready -funktio joten sen tulee olla index-sivulla. 
+	if (player_id == null) {
+		$("kirjautumiskentta").show();
+	}
+	});			
+				 */
+				 
+				 
+				 
+			
+	$("#submit_login").click(function(e) {  /*tekee sen että ei voi kirjautua tyhjällä käyttäjänimikentällä */
+        console.log("rairairai0");
+		if (($("#kayttaja").val() == "") || ($("#salasana").val() == "")) {
+			return false;				
+		} else {
+			console.log("rairairai");
+			setPlayer(($("#kayttaja").val()));
+			var b = document.getElementById("kirjautumiskentta")
+			b.style.display = "none";
+		};
+	});
+	
 
 
 });
